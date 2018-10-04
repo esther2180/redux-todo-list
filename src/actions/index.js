0a548a5a1039e2d22ba5fe4607aa79dcc5f39fb2
Esther <esther2180@gmail.com>
2018-10-03 17:09:17 -0700

    Completed getting list from server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
 import types from './types';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=c718todolistes';
+
+export function getListData() {
+    const resp = axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+    return {
+        type: types.GET_LIST_DATA,
+        payload: resp
+    }
+}
+
